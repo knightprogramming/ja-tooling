97db6d18d393ed5a4f911210317bb569ca057470
Administrator <Administrator@JA.local>
2018-10-19 11:03:33 -0600

    working inventory update

--- gpInteractions.js
+++ gpInteractions.js
@@ -16,18 +16,28 @@ const config = {
 
 module.exports = async () => {
     try {
-        const query = `SELECT a.ITEMNMBR,a.ITEMCODE,a.SELNGUOM,a.STNDCOST,a.MINSHELF1,b.QTYONHND,b.ATYALLOC,b.PRIMVNDR,c.PRCLEVEL,c.UOMPRICE
+        const query = `SELECT a.ITEMNMBR,a.ITEMCODE,a.SELNGUOM,a.STNDCOST,b.QTYONHND,b.ATYALLOC, c.PRCLEVEL,c.UOMPRICE
                         FROM IV00101 a
                             INNER JOIN IV00102 b
                                 ON a.ITEMNMBR = b.ITEMNMBR
                             INNER JOIN IV00108 c
-                                ON a.ITEMNMBR = c.ITEMNMBR`;
+                                ON a.ITEMNMBR = c.ITEMNMBR
+                            WHERE c.PRCLEVEL='RETAIL'`;
         const pool = await new sql.ConnectionPool(config);
         await pool.connect();
         let request = new sql.Request(pool);
         let results = await request.query(query);
         await pool.close();
-        return results;
+        return results.recordsets[0].reduce((acc, result) => {
+            if(!acc.ITEMNMBR){
+                acc[result.ITEMNMBR.trim()] = {
+                    sku: result.ITEMNMBR.trim(),
+                    stock: parseFloat(result.QTYONHND) - parseFloat(result.ATYALLOC),
+                };
+            }
+            acc[result.ITEMNMBR.trim()][result.PRCLEVEL.trim()] = result.UOMPRICE
+            return acc;
+        },{});
     } catch (err) {
         console.log(err);
     }
--- index.js
+++ index.js
@@ -3,10 +3,30 @@ const wp =  require('./wpInteractions');
 const fs = require('fs');
 
 (async () => {
-    // const product = await  wp.getInventory();
+    const product = await  wp.getInventory();
     const gpProduct = await require('./gpInteractions')();
 
     // fs.writeFileSync(`wcInventory${new Date().getUTCHours()}.json`,JSON.stringify(product));
-    fs.writeFileSync(`gpInventory${new Date().getUTCHours()}.json`,JSON.stringify(gpProduct, null, '\t'));
+    // fs.writeFileSync(`gpInventory${new Date().getUTCHours()}.json`,JSON.stringify(gpProduct, null, '\t'));
     
+    const update = product.reduce((acc, item)=> {
+        let gpItem = gpProduct[item.sku];
+        if(gpItem && (gpItem.RETAIL != item.price || gpItem.stock != item.stock_quantity)) {
+            item.stock_quantity = gpItem.stock.toString();
+            item.price = gpItem.RETAIL.toString();
+            item.manage_stock = true;
+            acc.push(item);
+        }
+        return acc;
+    },[]);
+    let ii = 0;
+    const task = setInterval(()=>{
+        const item = update[ii];
+        if(typeof item === 'undefined') return clearInterval(task);
+        wp.updateProduct(item.id, item);
+        console.log("Updating item: " + item.sku)
+        ii++;
+    }, 1000);
+    console.log("Finsed update.");
+    fs.writeFile("wcInvintoryUpdate.json", JSON.stringify(update),() => {});
 }) ();
